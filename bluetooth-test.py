39338513e1b21fb64d915a202862b476d8bc7b6a
tpawlenty <tristan-pawlenty@uiowa.edu>
2024-02-26 21:24:09 -0600

    Added bluetooth advertisement

--- bluetooth-test.py
+++ bluetooth-test.py
@@ -6,3 +6,15 @@ uuid = "128-bit random generated"
 
 # Initialize Bluetooth socket
 server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+port = 1  # RFCOMM port number
+server_socket.bind(("", port))
+server_socket.listen(1)
+
+# Advertise the service
+bluetooth.advertise_service(server_socket, service_name,
+                             service_id=uuid,
+                             service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                             profiles=[bluetooth.SERIAL_PORT_PROFILE])
+
+print("Waiting for connection on RFCOMM channel", port)
